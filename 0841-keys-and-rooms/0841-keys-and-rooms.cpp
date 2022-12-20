c3d279f50b41a41ade740c158d7e60f2bf8ff0d7
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2022-12-20 09:51:01 +0530

    Time: 23 ms (34.07%), Space: 10.4 MB (80.63%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,65 +1,65 @@
-// //simple bfs
-// class Solution {
-// public:
-//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-//         int n = rooms.size();
-//         vector<int>vis(n,0);
+//simple bfs
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<int>vis(n,0);
 
-//         queue<int>q;
-//         q.push(0);
-//         while(!q.empty())
-//         {
-//             int node = q.front();
-//             q.pop();
-//             vis[node] = 1;
-//             for(auto  adjNode : rooms[node])
-//             {
-//                 if(!vis[adjNode])
-//                 {
-//                     q.push(adjNode);
-//                     vis[adjNode] = 1;
-//                 }
+        queue<int>q;
+        q.push(0);
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            vis[node] = 1;
+            for(auto  adjNode : rooms[node])
+            {
+                if(!vis[adjNode])
+                {
+                    q.push(adjNode);
+                    vis[adjNode] = 1;
+                }
                 
-//             }
-//         }
-//         for(int i = 0; i< n; i++)
-//         {
-//             if(vis[i] == 0)
-//             return false;
-//         }
-//         return true;
-//     }
-// };
+            }
+        }
+        for(int i = 0; i< n; i++)
+        {
+            if(vis[i] == 0)
+            return false;
+        }
+        return true;
+    }
+};
 
 
 //simple dfs solution
 
-class Solution {
-public:
+// class Solution {
+// public:
 
-    void dfs(int node , vector<int>&vis,vector<vector<int>>&rooms,int n)
-    {
-        vis[node] = 1;
-        for(auto adjNode : rooms[node])
-        {
-            if(!vis[adjNode])
-            {
-                dfs(adjNode,vis,rooms,n);
-            }
-        }
-    }
+//     void dfs(int node , vector<int>&vis,vector<vector<int>>&rooms,int n)
+//     {
+//         vis[node] = 1;
+//         for(auto adjNode : rooms[node])
+//         {
+//             if(!vis[adjNode])
+//             {
+//                 dfs(adjNode,vis,rooms,n);
+//             }
+//         }
+//     }
 
 
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n = rooms.size();
-        vector<int>vis(n,0);
-               dfs(0,vis,rooms,n);
+//     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+//         int n = rooms.size();
+//         vector<int>vis(n,0);
+//                dfs(0,vis,rooms,n);
 
-     for(int i = 0; i< n;i++)
-     {
-         if(vis[i] == 0)
-         return false;
-     }
-     return true;
-    }
-};
+//      for(int i = 0; i< n;i++)
+//      {
+//          if(vis[i] == 0)
+//          return false;
+//      }
+//      return true;
+//     }
+// };
