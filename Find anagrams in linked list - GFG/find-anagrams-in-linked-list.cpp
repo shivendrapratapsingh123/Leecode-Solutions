8a8772de1a8349a7ed64ecd48dffbf55c35d0d2a
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-03-10 22:09:40 +0530

    Added solution - LeetHub

---
+++ Find anagrams in linked list - GFG/find-anagrams-in-linked-list.cpp
@@ -0,0 +1,127 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    char data;
+    struct Node *next;
+
+    Node(char x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+struct Node *inputList() {
+    int n; // length of link list
+    scanf("%d ", &n);
+
+    char data;
+    cin >> data;
+    struct Node *head = new Node(data);
+    struct Node *tail = head;
+    for (int i = 0; i < n - 1; ++i) {
+        cin >> data;
+        tail->next = new Node(data);
+        tail = tail->next;
+    }
+    return head;
+}
+
+
+// } Driver Code Ends
+/*
+
+Definition for singly Link List Node
+struct Node
+{
+    char data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+};
+
+You can also use the following for printing the link list.
+printList(Node* node);
+*/
+
+class Solution {
+  public:
+    vector<Node*> findAnagrams(struct Node* head, string s) {
+        
+        vector<int>stHash(26,0),sw(26,0);
+         vector<Node*>ans;
+        Node* temp = head;
+        Node* t = head;
+        int cnt = 0;
+        for(int i = 0;i<s.size();i++)
+        {
+            stHash[s[i]-'a']++;
+        }
+        while(temp != NULL)
+        {
+            sw[temp -> data - 'a']++;
+            //cout<<sw[temp->data -'a']<<" ";
+            //cout<<temp -> data<<" ";
+            cnt++;
+            if(cnt == s.size())
+            {
+                if(stHash == sw)
+                {
+                    ans.push_back(head);
+                    head = temp -> next;
+                    temp -> next = NULL;
+                    temp = head;
+                    sw.assign(26,0);
+                    cnt = 0;
+                }
+                else{
+                    sw[head -> data - 'a']--;
+                    head = head -> next;
+                    temp = temp -> next;
+                    cnt--;
+                }
+    
+            }
+            else{
+              temp = temp -> next;
+            }
+        }
+       // cout<<ans.size()<<" ";
+       //cout<<endl;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        struct Node *head = inputList();
+
+        string s;
+        cin >> s;
+
+        Solution obj;
+        vector<Node *> res = obj.findAnagrams(head, s);
+
+        for (int i = 0; i < res.size(); i++) {
+            printList(res[i]);
+        }
+
+        if (res.size() == 0) cout << "-1\n";
+    }
+}
+
+// } Driver Code Ends
