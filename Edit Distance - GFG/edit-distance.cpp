a731d1e054a0bbd96ca188add1da8f16d2c3eb01
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-05-24 23:32:02 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -6,35 +6,34 @@ using namespace std;
 class Solution {
   public:
    
-   int solve(int ind1, int ind2 , string& s, string& t,vector<vector<int>>&memo){
-       if(ind1 < 0 )
-       return ind2+1;
-       if(ind2 < 0) 
-       return ind1+1;
-      
-       if(memo[ind1][ind2] != -1)
-       return memo[ind1][ind2];
-      
-       if(s[ind1] == t[ind2])
-       {
-           memo[ind1][ind2] = 0 + solve(ind1-1,ind2-1,s,t,memo);
-       }
-       else{
-           int x =  1 + solve(ind1-1,ind2,s,t,memo);
-           int y  = 1 + solve(ind1,ind2-1,s,t,memo);
-           int z =  1 + solve(ind1-1,ind2-1,s,t,memo);
-           memo[ind1][ind2] = min({x,y,z});
-       }
-       return memo[ind1][ind2];
-     
-   }
+
+  // tabulation space optimisation
   
     int editDistance(string s, string t) {
     int n = s.size();
     int m = t.size();
-    vector<vector<int>>memo(n+1,vector<int>(m+1,-1));
-    return solve(n-1,m-1,s,t,memo);
-     
+   vector<int>prev(m+1,0),curr(m+1,0);
+    
+    for(int j = 0;j<=m;j++)
+    prev[j] = j;
+   
+    for(int i = 1;i<=n;i++)
+    {
+        curr[0] = i;
+        for(int j = 1;j<=m;j++)
+        {
+            if(s[i-1] == t[j-1]) 
+            curr[j] = prev[j-1];
+            else{
+          int x =  1 + prev[j];
+          int y  =  1 + curr[j-1];
+          int z =  1 + prev[j-1];
+          curr[j] = min({x,y,z}); 
+            }
+        }
+        prev = curr;
+    }
+    return prev[m];
     }
 };
 
