0e81a0344f4156fd80db97274e8cdddc747d779c
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-05-25 12:37:00 +0530

    Added solution - LeetHub

--- Friends Pairing Problem - GFG/friends-pairing-problem.cpp
+++ Friends Pairing Problem - GFG/friends-pairing-problem.cpp
@@ -17,11 +17,19 @@ public:
          long long pair = (n-1)*solve(n-2,dp);
          return dp[n] = ((single%mod) + (pair%mod))%mod;
      }
-    
+    // taulation 
     int countFriendsPairings(int n) 
     { 
-        vector<long long>dp(n+1,-1);
-        return solve(n,dp);
+        vector<long long>dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i<=n;i++)
+        {
+            dp[i] = ((dp[i-1]%mod)+ ((i-1)*dp[i-2]%mod))%mod;
+        }
+        return (int)dp[n];
     }
 };    
  
