2ebf267e1345ba42c890f5e1538c83ba3ae3c37d
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-05-29 21:42:30 +0530

    Added solution - LeetHub

--- Pairs with certain difference - GFG/pairs-with-certain-difference.cpp
+++ Pairs with certain difference - GFG/pairs-with-certain-difference.cpp
@@ -6,25 +6,34 @@ using namespace std;
 class Solution{
     public:
     
-    int solve( int ind, int k,int arr[],vector<int>&dp)
-    {
-        if(ind <=0)
-        return 0;
-        if(dp[ind] != -1) 
-        return dp[ind];
-        int notTake = solve(ind-1,k,arr,dp);
-        int take   = 0;
-        if(  arr[ind] - arr[ind-1] < k)
-        take =  arr[ind] + arr[ind-1] + solve(ind-2,k,arr,dp);
-        return dp[ind] = max(notTake, take);
-    }
+    // int solve( int ind, int k,int arr[],vector<int>&dp)
+    // {
+    //     if(ind <=0)
+    //     return 0;
+    //     if(dp[ind] != -1) 
+    //     return dp[ind];
+    //     int notTake = solve(ind-1,k,arr,dp);
+    //     int take   = 0;
+    //     if(  arr[ind] - arr[ind-1] < k)
+    //     take =  arr[ind] + arr[ind-1] + solve(ind-2,k,arr,dp);
+    //     return dp[ind] = max(notTake, take);
+    // }
     
-    
+    // tabulation 
     int maxSumPairWithDifferenceLessThanK(int arr[], int n, int k)
     {
         sort(arr,arr+n);
-        vector<int>dp(n,-1);
-        return solve(n-1,k,arr,dp);
+        vector<int>dp(n,0);
+        for(int i = 1;i<n;i++)
+        {
+            int notTake = dp[i-1];
+            int take = 0;
+            if(arr[i]-arr[i-1] < k)
+            take = arr[i] + arr[i-1] + dp[i-2];
+            dp[i] = max(notTake , take );
+        }
+         return dp[n-1];
+        //return solve(n-1,k,arr,dp);
     }
 };
 
