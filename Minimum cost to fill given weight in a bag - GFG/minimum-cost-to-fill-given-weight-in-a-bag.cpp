35c2084c80652becfce3696ff3dd1dd2de7ef550
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-05-30 13:32:58 +0530

    Added solution - LeetHub

--- Minimum cost to fill given weight in a bag - GFG/minimum-cost-to-fill-given-weight-in-a-bag.cpp
+++ Minimum cost to fill given weight in a bag - GFG/minimum-cost-to-fill-given-weight-in-a-bag.cpp
@@ -9,26 +9,41 @@ using namespace std;
 
 class Solution{
 	
-	int solve(int n,int w, int cost[],vector<vector<int>>&dp)
-	{
-	    if(n == 0 && w)
-	    return 1e8;
-	    if(w == 0)
-	    return 0;
-	    if(dp[n][w] != -1)
-	    return dp[n][w];
-	    int notTake = solve(n-1,w,cost,dp);
-	    int take = 1e8;
-	    if(cost[n-1] != -1 && w - n >=0)
-	    take = cost[n-1] + solve(n,w-n,cost,dp);
-	    return dp[n][w] = min(take,notTake);
-	}
+// 	int solve(int n,int w, int cost[],vector<vector<int>>&dp)
+// 	{
+// 	    if(n == 0 && w)
+// 	    return 1e8;
+// 	    if(w == 0)
+// 	    return 0;
+// 	    if(dp[n][w] != -1)
+// 	    return dp[n][w];
+// 	    int notTake = solve(n-1,w,cost,dp);
+// 	    int take = 1e8;
+// 	    if(cost[n-1] != -1 && w - n >=0)
+// 	    take = cost[n-1] + solve(n,w-n,cost,dp);
+// 	    return dp[n][w] = min(take,notTake);
+// 	}
      
 	public:
+	// tabulation 
 	int minimumCost(int cost[], int N, int W) 
 	{ 
-	    vector<vector<int>>dp(N+1,vector<int>(W+1,-1));
-      int res =  solve(N,W,cost,dp);
+	    //vector<vector<int>>dp(N+1,vector<int>(W+1,-1));
+	    vector<vector<int>>dp(N+1,vector<int>(W+1,0));
+	    for(int wt = 0;wt<=W;wt++)
+	    dp[0][wt] = 1e8;
+	    for(int ind = 1;ind <= N ; ind++)
+	    {
+	        for(int wt = 1;wt <= W;wt++)
+	        {
+	            int notTake = dp[ind-1][wt];
+	            int take = 1e8;
+	            if(cost[ind-1] != -1 && wt -ind >=0)
+	            take = cost[ind-1] + dp[ind][wt-ind];
+	            dp[ind][wt] = min(take,notTake);
+	        }
+	    }
+      int res =  dp[N][W];
       if(res >= 1e8)
       return -1;
       return res;
