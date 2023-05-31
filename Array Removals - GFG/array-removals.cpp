0e79d884b534855b78b8773e7486210be4292995
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-05-31 09:36:37 +0530

    Added solution - LeetHub

--- Array Removals - GFG/array-removals.cpp
+++ Array Removals - GFG/array-removals.cpp
@@ -10,26 +10,18 @@ using namespace std;
 
 class Solution{
     public:
-    
-    int solve(int i, int j, vector<int>&arr,int k,vector<vector<int>>&dp)
-    {
-        if(i >= j || arr[j] - arr[i] <= k)
-        return 0;
-        if(dp[i][j] != -1)
-        return dp[i][j];
-        if(arr[j]-arr[i] > k)
-        {
-            int removeI = 1 +  solve(i+1,j,arr,k,dp);
-            int removeJ = 1  + solve(i,j-1,arr,k,dp);
-             return dp[i][j] =  min(removeI,removeJ);
-        }
-    }
-    
+    //binary search approach 
     int removals(vector<int>& arr, int k){
-        sort(arr.begin(),arr.end());
+        
         int n = arr.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-      return solve(0,n-1,arr,k,dp);
+        int ans = n-1;
+        sort(arr.begin(),arr.end());
+        for(int i = 0;i<n;i++)
+        {
+            int pos = upper_bound(arr.begin(),arr.end(),arr[i]+k) - arr.begin();
+            ans = min(ans,n - (pos-i));
+        }
+        return  ans;
     }
 };
 
