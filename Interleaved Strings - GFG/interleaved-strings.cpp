273acf9f61584108d8a6d24a74c7b719bc6e38ee
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-06-04 00:34:05 +0530

    Added solution - LeetHub

--- Interleaved Strings - GFG/interleaved-strings.cpp
+++ Interleaved Strings - GFG/interleaved-strings.cpp
@@ -10,24 +10,48 @@ using namespace std;
 class Solution{
   public:
     /*You are required to complete this method */
-    int solve(string& s1, string& s2, string& s3,int i, int j,map<pair<int,int>,bool>&dp)
+    // int solve(string& s1, string& s2, string& s3,int i, int j,map<pair<int,int>,bool>&dp)
+    // {
+    //     if(i == s1.size() && j == s2.size())
+    //     return true;
+    //     if(dp.find({i,j}) != dp.end())
+    //     return dp[{i,j}];
+    //     if( i < s1.size() && s1[i] == s3[i+j] && solve(s1,s2,s3,i+1,j,dp))
+    //     return true;
+    //     if( j < s2.size() && s2[j] == s3[i+j] && solve(s1,s2,s3,i,j+1,dp))
+    //     return true;
+    //     return dp[{i,j}] =  false;
+    // }
+    bool isInterleave(string s1, string s2, string s3) 
     {
-        if(i == s1.size() && j == s2.size())
-        return true;
-        if(dp.find({i,j}) != dp.end())
-        return dp[{i,j}];
-        if( i < s1.size() && s1[i] == s3[i+j] && solve(s1,s2,s3,i+1,j,dp))
-        return true;
-        if( j < s2.size() && s2[j] == s3[i+j] && solve(s1,s2,s3,i,j+1,dp))
-        return true;
-        return dp[{i,j}] =  false;
-    }
-    bool isInterleave(string A, string B, string C) 
-    {
-        if(A.size() + B.size() != C.size())
+        if(s1.size() + s2.size() != s3.size())
         return false;
-        map<pair<int,int>,bool>dp;
-        return solve(A,B,C,0,0,dp);
+         int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+     
+        dp[n][m] = true;
+        for(int i = m-1;i>=0;i--)
+        {
+            if(s2[i] == s3[i+n])
+            dp[n][i] = true;
+        }
+        for(int i = n-1;i>=0;i--)
+        {
+            if(s1[i] == s3[i+m])
+            dp[i][m] = true;
+        }
+        for(int i = n-1;i>=0;i--)
+        {
+            for(int j = m-1;j>=0;j--)
+            {
+                if(s1[i] == s3[i+j] && dp[i+1][j])
+                dp[i][j] = true;
+                if(s2[j] == s3[i+j] && dp[i][j+1])
+                dp[i][j] = true;
+            }
+        }
+        return dp[0][0];
     }
 
 };
