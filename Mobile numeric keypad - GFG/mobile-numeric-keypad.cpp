775bfd524ff1e5d5fcb9ae35bdf08fafeba02e32
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-06-04 23:21:45 +0530

    Added solution - LeetHub

--- Mobile numeric keypad - GFG/mobile-numeric-keypad.cpp
+++ Mobile numeric keypad - GFG/mobile-numeric-keypad.cpp
@@ -7,39 +7,64 @@ class Solution{
 
 
 	public:
+	
+	
+	long long getPresses(int r, int c, int n,int count,vector<int>&dr,vector<int>&dc, vector<vector<int>>&keypaid,vector<vector<long long int>>&dp){
+	  
+	  if(count == n)
+	  return 1;
+	  if(dp[count][keypaid[r][c]] != -1)
+	  return dp[count][keypaid[r][c]];
+	   long long int ans = 0;
+	   for(int i = 0;i<dr.size();i++){
+	       int tr = r + dr[i];
+	       int tc = c + dc[i];
+	       if(tr >= 0 && tr < keypaid.size() && tc >=0 && tc < keypaid[0].size() && keypaid[tr][tc] != -1)
+	       {
+	           ans += getPresses(tr,tc,n,count+1,dr,dc,keypaid,dp);
+	       }
+	   }
+	   return dp[count][keypaid[r][c]] =  ans;
+	}
+	
+	//tabulation 
 	long long getCount(int N)
 	{
-       vector<vector<int>>graph;
-       graph.push_back({0,8});
-      graph.push_back({1,2,4});
-      graph.push_back({1,2,3,5});
-      graph.push_back({2,3,6});
-      graph.push_back({1,4,5,7});
-      graph.push_back({2,4,5,6,8});
-      graph.push_back({3,5,6,9});
-      graph.push_back({4,7,8});
-      graph.push_back({5,7,8,9,0});
-      graph.push_back({6,8,9});
-       
-       vector<vector<long long int >>dp(N+1,vector<long long int>(10,0));
-       for(int i = 1;i<=N;i++)
-       {
-            for(int j = 0;j<=9;j++)
-            {
-                if(i == 1)
-                {
-                    dp[i][j] = 1;
-                }
-                else{
-                    for(int prev : graph[j])
-                    dp[i][j] += dp[i-1][prev];
-                }
-            }
-       }
-       long long int  sum = 0;
-       for(int i = 0;i<=9;i++)
-       sum += dp[N][i];
-      return sum;
+		vector<vector<int>>keypaid{
+		    {1,2,3},
+		    {4,5,6},
+		    {7,8,9},
+		    {-1,0,-1}
+		};
+		
+		vector<int>dr{0,-1,0,1,0};
+		vector<int>dc{0,0,1,0,-1};
+		vector<vector<long long int>>dp(N+1,vector<long long int>(10,0));
+		for(int i = 0;i<10;i++)
+		dp[1][i] = 1;
+		long long ans  = 0;
+		int r,c,count = 0;
+		for(int cnt = 2;cnt <= N; cnt++)
+		{
+		   	for(int r = 0;r<keypaid.size();r++)
+	    	{
+		    for(int c = 0;c<keypaid[0].size();c++){
+		        if(keypaid[r][c] != -1){
+		             for(int x = 0;x<dr.size();x++){
+	                 int tr = r + dr[x];
+	                 int tc = c + dc[x];
+	               if(tr >= 0 && tr < keypaid.size() && tc >=0 && tc < keypaid[0].size() && keypaid[tr][tc] != -1)
+	                {
+	                   dp[cnt][keypaid[r][c]] += dp[cnt-1][keypaid[tr][tc]];
+	                }
+	          }
+		        }
+		      }
+	     	}   
+		}
+	  for(int i = 0;i<10;i++)
+	    ans += dp[N][i];
+	    return ans;
 	}
 
 
