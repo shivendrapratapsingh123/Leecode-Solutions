5491412e8e19c35415f764965f1bc6a0d77181e8
Shivendra Pratap Singh <shivendra.sp.123@gmail.com>
2023-06-04 17:31:09 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -12,66 +12,66 @@ public:
     
     int mod = 1003;
   
-  int solve(int i, int j, int isTrue,string& s, vector<vector<vector<int>>>&dp)
-    {
-      if( i > j)
-      return 0;
+//   int solve(int i, int j, int isTrue,string& s, vector<vector<vector<int>>>&dp)
+//     {
+//       if( i > j)
+//       return 0;
       
-      if( i == j)
-      {
-          if(isTrue == 1)
-          {
-              return s[i] == 'T';
-          }
-          else{
-              return s[i] == 'F';
-          }
-      }
-      if(dp[i][j][isTrue] != -1)
-      return dp[i][j][isTrue];
-      int ways = 0;
-      for(int ind = i+1; ind <= j-1; ind += 2){
+//       if( i == j)
+//       {
+//           if(isTrue == 1)
+//           {
+//               return s[i] == 'T';
+//           }
+//           else{
+//               return s[i] == 'F';
+//           }
+//       }
+//       if(dp[i][j][isTrue] != -1)
+//       return dp[i][j][isTrue];
+//       int ways = 0;
+//       for(int ind = i+1; ind <= j-1; ind += 2){
          
-         int lT = solve(i,ind-1,1,s,dp);
-         int lF = solve(i, ind-1,0,s,dp);
-         int rT = solve(ind+1,j,1,s,dp);
-         int rF = solve(ind+1,j,0,s,dp);
+//          int lT = solve(i,ind-1,1,s,dp);
+//          int lF = solve(i, ind-1,0,s,dp);
+//          int rT = solve(ind+1,j,1,s,dp);
+//          int rF = solve(ind+1,j,0,s,dp);
         
-         if(s[ind] == '&'){
+//          if(s[ind] == '&'){
              
-             if(isTrue)
-             {
-                 ways = (ways%mod +  (lT*rT)%mod)%mod;
-             }
-             else{
-                 ways = (ways%mod +  (lT*rF)%mod + (lF*rT)%mod + (lF*rF)%mod)%mod;
-             }
-         }
-         else if(s[ind] == '|')
-        {
-            if(isTrue)
-            {
-                ways =( ways%mod + (lT*rF)%mod + (lF*rT)%mod + (lT*rT)%mod)%mod;
-            }
-            else{
-                ways = (ways%mod + (lF*rF)%mod)%mod;
-            }
+//              if(isTrue)
+//              {
+//                  ways = (ways%mod +  (lT*rT)%mod)%mod;
+//              }
+//              else{
+//                  ways = (ways%mod +  (lT*rF)%mod + (lF*rT)%mod + (lF*rF)%mod)%mod;
+//              }
+//          }
+//          else if(s[ind] == '|')
+//         {
+//             if(isTrue)
+//             {
+//                 ways =( ways%mod + (lT*rF)%mod + (lF*rT)%mod + (lT*rT)%mod)%mod;
+//             }
+//             else{
+//                 ways = (ways%mod + (lF*rF)%mod)%mod;
+//             }
           
-        }
-        else if(s[ind] == '^')
-        {
-            if(isTrue)
-            {
-                ways = (ways%mod +  (lT*rF)%mod + (lF*rT)%mod)%mod;
-            }
-            else{
-                ways = (ways%mod +  (lT*rT)%mod + (lF*rF)%mod)%mod;
-            }
-        }
+//         }
+//         else if(s[ind] == '^')
+//         {
+//             if(isTrue)
+//             {
+//                 ways = (ways%mod +  (lT*rF)%mod + (lF*rT)%mod)%mod;
+//             }
+//             else{
+//                 ways = (ways%mod +  (lT*rT)%mod + (lF*rF)%mod)%mod;
+//             }
+//         }
           
-      }
-      return dp[i][j][isTrue] =  ways%mod;
-    }
+//       }
+//       return dp[i][j][isTrue] =  ways%mod;
+//     }
     
     // tabulation 
     int countWays(int N, string s){
